6f7112c8c8de0f70a5cbf4965b2c42220c0bd641
Mona Salih <monaalobisat@gmail.com>
2025-01-03 01:01:21 +0300

    upload json work

---
+++ main.js
@@ -0,0 +1,14 @@
+'use strict'
+
+let jsonData;
+fetch("product.json").then(
+    res=>{      
+        return res.json();
+    }
+).then(data=>{
+    
+    jsonData=data;
+    
+    console.log(jsonData,"data");
+})
+// console.log(jsonData,"jsonData");
